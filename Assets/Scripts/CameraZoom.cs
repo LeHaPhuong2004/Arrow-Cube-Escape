ec8e2b05d3a7b85bea55e942482a69ad69eef405
phuonghale <phuonghale0210@gmail.com>
2026-04-17 20:35:22 +0700

    Them zoomin/out, tuongtac tren android

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float zoomSpeedMouse = 10f;
+    public float zoomSpeedTouch = 0.01f; 
+
+    public float minZoom = 5f;
+    public float maxZoom = 10f;
+
+    public float minFOV = 35f;
+    public float maxFOV = 95f;
+
+    Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+
+        if (cam == null)
+        {
+            Debug.LogError("No cam");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.touchCount >= 2)
+        {
+            HandleTouchZoom();
+        }
+        else
+        {
+            HandleMouseZoom();
+        }
+    }
+
+    void HandleMouseZoom()
+    {
+        float scroll = Input.mouseScrollDelta.y;
+
+        if (Mathf.Abs(scroll) > 0.01f)
+        {
+            ApplyZoom(scroll * zoomSpeedMouse);
+        }
+    }
+
+    void HandleTouchZoom()
+    {
+        Touch t0 = Input.GetTouch(0);
+        Touch t1 = Input.GetTouch(1);
+
+        Vector2 t0Prev = t0.position - t0.deltaPosition;
+        Vector2 t1Prev = t1.position - t1.deltaPosition;
+
+        float prevDist = Vector2.Distance(t0Prev, t1Prev);
+        float currDist = Vector2.Distance(t0.position, t1.position);
+
+        float delta = currDist - prevDist;
+
+        ApplyZoom(delta * zoomSpeedTouch);
+    }
+
+    void ApplyZoom(float delta)
+    {
+        if (cam.orthographic)
+        {
+            cam.orthographicSize += delta;
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minZoom, maxZoom);
+        }
+        else
+        {
+            cam.fieldOfView += delta;
+            cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, minFOV, maxFOV);
+        }
+    }
+}
